6a78b912c8892bdb631ccbfcae900e97f7198cb9
Raoul Rapeli <raoul.rapeli@gmail.com>
2024-01-15 18:38:40 +0200

    Part3_1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "number": "040-123456",
+        "name": "Arto Hellas",
+        "id": "1"
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": "2"
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": "3"
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": "4"
+      }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.end(JSON.stringify(persons))
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
