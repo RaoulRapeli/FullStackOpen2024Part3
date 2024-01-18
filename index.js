967f4f2ac180a2a47b10216190ce9bf3f77ca451
Raoul Rapeli <raoul.rapeli@gmail.com>
2024-01-18 20:07:48 +0200

    Part3_19

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ morgan.token('body', function getBody (req) {
   })
 
 app.use(express.json())
+
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
@@ -21,7 +22,7 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -43,21 +44,16 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id,{ name, number },{ new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
         response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
   
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -78,6 +74,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -103,6 +100,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
